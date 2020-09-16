9b44fcea2a4740c58148834943da144b4ba00fc6
Christian Oder <myself5@carbonrom.org>
2020-09-16 02:06:46 +0200

    recovery: Make recovery usable on user builds
    Remove restrictions introduced for user builds of recovery.
    
    Change-Id: Ib84de6e1fc1d5874a3bd993bd3ec581c07890ef8

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -504,6 +504,7 @@ int main(int argc, char** argv) {
   if (get_build_type() == "user") {
     device->RemoveMenuItemForAction(Device::WIPE_SYSTEM);
     device->RemoveMenuItemForAction(Device::MOUNT_SYSTEM);
+  }
   
   if (!android::base::GetBoolProperty("ro.build.ab_update", false)) {
     device->RemoveMenuItemForAction(Device::SWAP_SLOT);
