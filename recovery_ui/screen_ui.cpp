976a11bcc8306d71510e97e5133989afd2a3e5f2
Chara <bionichuman07@gmail.com>
2026-04-07 16:57:23 -0400

    Revert "EXPERIMENTAL: Make text white ONLY when selected"
    This reverts commit b6164794efd35dfa26ba52f93cd87cec3d0e6ca1.
    Borked it.

--- recovery_ui/screen_ui.cpp
+++ recovery_ui/screen_ui.cpp
@@ -66,9 +66,6 @@ constexpr uint8_t kLightBgB = 0xff;
 constexpr uint8_t kLightTextR = 0xb1;
 constexpr uint8_t kLightTextG = 0x12;
 constexpr uint8_t kLightTextB = 0x26;
-constexpr uint8_t kLightSelectedTextR = 0xff;
-constexpr uint8_t kLightSelectedTextG = 0xff;
-constexpr uint8_t kLightSelectedTextB = 0xff;
 constexpr uint8_t kLightHighlightR = 0xb1;
 constexpr uint8_t kLightHighlightG = 0x12;
 constexpr uint8_t kLightHighlightB = 0x26;
@@ -811,11 +808,7 @@ void ScreenRecoveryUI::SetColor(UIElement e) const {
       case UIElement::INFO:
       case UIElement::MENU:
       case UIElement::LOG:
-        if (is_selected) {
-          gr_color(kLightSelectedTextR, kLightSelectedTextG, kLightSelectedTextB, 255);
-        } else {
-          gr_color(kLightTextR, kLightTextG, kLightTextB, 255);
-        }
+        gr_color(kLightTextR, kLightTextG, kLightTextB, 255);
         break;
       case UIElement::HEADER:
         gr_color(kHeaderLightR, kHeaderLightG, kHeaderLightB, 255);
@@ -831,21 +824,13 @@ void ScreenRecoveryUI::SetColor(UIElement e) const {
         gr_color(kLightHighlightR, kLightHighlightG, kLightHighlightB, 255);
         break;
       case UIElement::MENU_SEL_FG:
-        if (is_selected) {
-          gr_color(kLightSelectedTextR, kLightSelectedTextG, kLightSelectedTextB, 255);
-        } else {
-          gr_color(kLightTextR, kLightTextG, kLightTextB, 255);
-        }
+        gr_color(kLightTextR, kLightTextG, kLightTextB, 255);
         break;
       case UIElement::TEXT_FILL:
         gr_color(255, 255, 255, 160);
         break;
       default:
-        if (is_selected) {
-          gr_color(kLightSelectedTextR, kLightSelectedTextG, kLightSelectedTextB, 255);
-        } else {
-          gr_color(kLightTextR, kLightTextG, kLightTextB, 255);
-        }
+        gr_color(kLightTextR, kLightTextG, kLightTextB, 255);
         break;
     }
     return;
